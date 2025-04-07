8be7fc34a5096dc37ffcb9644dafb4e0b12ee8fb
hiren-kavad <hirenwork7@gmail.com>
2025-04-07 16:46:31 +0100

    started task 2

---
+++ task2/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 id='main_heading'>Daily Tasks Manager</h1> 
+      <div id='main_container'>
+
+        
+
+      </div>
+    </>
+  )
+}
+
+export default App
